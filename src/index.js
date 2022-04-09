8233e53dc91906f81121eb3787692464c6889273
Ikky <rezkytaewa2@gmail.com>
2022-04-09 22:29:54 +0700

    test3

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Button } from 'reactstrap';
-// import { Home } from './Components/Home';
-// import { Contact } from './Components/Contact';
-// import { Skill } from './Components/Skill';
-// import { Question } from './Components/Question';
-// import { Intro } from './Components/Intro';
-
-
-
 
 ReactDOM.render(
   <React.StrictMode>  
@@ -24,8 +15,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
